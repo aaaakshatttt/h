e18fa5c8c9a473c9da2b02e9741e310ea5eff45b
aaaakshatttt <veeakshat4@gmail.com>
2026-09-04 18:47:31 +0530

    h

--- script2.js
+++ script2.js
@@ -1,18 +1,38 @@
-let getstrawberries=()=>{
-    return new Promise((resolve,reject)=>{
-        setTimeout(() => {
-            resolve("red fucking strawberries")
-        }, 2000);
-    })
-}
-
-async function makesmoothie() {
-    console.log("open blender");
-
-    let strawberries = await getstrawberries();
-
-    console.log(`here us your ${strawberries} mf`)
+function cherry() {
+    console.log("i aint waiting");
     
 }
-makesmoothie();
-console.log("tv is playing")
+async function harry() {
+    let delhiweather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("22 degree")
+        }, 2000);
+    })
+    let lkoweather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("couldn't get")
+        }, 5000);
+    })
+
+    console.log("fetching delhi weather");
+    let delhiw = await delhiweather
+    console.log("fetched weateher is" + delhiw);
+    console.log("fetching lko weather");
+    try{
+    let lkow = await lkoweather
+    console.log("fteched watech is " + lkow);}
+    catch(error){
+        console.log("fuck " + error);
+        
+    }
+    
+    
+    
+    
+    
+}
+console.log("welcome to weather control");
+
+harry()
+
+cherry();
